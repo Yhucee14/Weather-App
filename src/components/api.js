42129f9da047421777661fe29cff2cc2de50eda8
Yhucee14 <uchennachukwu2002@yahoo.com>
2022-12-25 04:49:47 -0800

    Created a few components

---
+++ src/components/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'dd1c63c74emshac0209891fcc0f5p1db0e9jsn898ed874358a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+    export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
